ffb31624c277cfe8e865aa0805a29c644ab9278d
Youssef <youssefabdelmoniem11@gmail.com>
2026-05-07 22:11:40 +0300

    Feat: add search endpoint

--- app/db/vector_store.py
+++ app/db/vector_store.py
@@ -2,15 +2,23 @@ from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from app.core.config import settings
 
+# Cache embedding (avoid reloading model every time)
+_embedding = None
 
 def get_embedding():
-    return HuggingFaceEmbeddings(
-        model_name=settings.EMBEDDING_MODEL
-    )
+    global _embedding
+
+    if _embedding is None:
+        _embedding = HuggingFaceEmbeddings(
+            model_name=settings.EMBEDDING_MODEL
+        )
+
+    return _embedding
 
 
-def get_vector_store():
+def get_vector_store(collection_name: str = "documents") :
     return Chroma(
         persist_directory=settings.CHROMA_DIR,
-        embedding_function=get_embedding()
+        embedding_function=get_embedding(),
+        collection_name=collection_name
     )
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.routes import chat
 from app.api.routes import search
 from app.api.routes import documents
 
@@ -7,6 +8,8 @@ app = FastAPI()
 
 app.include_router(documents.router, prefix="/documents")
 app.include_router(search.router, prefix="/search")
+
+app.include_router(chat.router, prefix="/api")
 
 
 @app.get("/health")
